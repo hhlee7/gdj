bde99d30e9470263f8e3860b2781a2411c400948
hhlee7 <gdjlhh777@gmail.com>
2025-05-12 17:50:09 +0900

    250512 수업
    AJax
    - CSR / SSR
    - 대륙, 나라, 도시 선택 예제 실습 진행중

---
+++ ajax/src/main/java/com/example/ajax/controller/ContinentController.java
@@ -0,0 +1,35 @@
+package com.example.ajax.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.example.ajax.mapper.ContinentMapper;
+
+@Controller
+public class ContinentController {
+	@Autowired ContinentMapper continentMapper;
+	
+	@GetMapping({"/", "/continentList"})
+	public String continentList(@RequestParam(required = false) Integer continent,
+								@RequestParam(required = false) Integer country,
+								Model model) {
+		
+		// 대륙 목록 출력
+		model.addAttribute("continentList", continentMapper.selectContinentList());
+		
+		// 대륙 선택 시 나라 목록 조회
+		if(continent != null) {
+			model.addAttribute("countryList", continentMapper.selectCountryList());
+		}
+		
+		// 나라 선택 시 도시 목록 조회
+		if(country != null) {
+			model.addAttribute("cityList", continentMapper.selectCityList());
+		}
+		
+		return "continentList";
+	}
+}
---
+++ ajax/src/main/java/com/example/ajax/mapper/ContinentMapper.java
@@ -0,0 +1,13 @@
+package com.example.ajax.mapper;
+
+import java.util.List;
+import java.util.Map;
+
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface ContinentMapper {
+	List<Map<String, Object>> selectContinentList();
+	List<Map<String, Object>> selectCountryList();
+	List<Map<String, Object>> selectCityList();
+}
