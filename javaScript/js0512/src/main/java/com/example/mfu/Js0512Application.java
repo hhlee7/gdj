b0b3ed8d5d1d9148349e76b869c33c30dc3ec797
hhlee7 <gdjlhh777@gmail.com>
2025-05-12 13:35:12 +0900

    프로젝트 생성

---
+++ javaScript/js0512/src/main/java/com/example/mfu/Js0512Application.java
@@ -0,0 +1,13 @@
+package com.example.mfu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Js0512Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Js0512Application.class, args);
+	}
+
+}
