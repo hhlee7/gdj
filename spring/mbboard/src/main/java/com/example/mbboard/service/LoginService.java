438a6c66600b49de3a4146480577c98026c738d3
hhlee7 <gdjlhh777@gmail.com>
2025-05-15 15:44:57 +0900

    철자 오류 수정

--- spring/mbboard/src/main/java/com/example/mbboard/service/LoginService.java
+++ spring/mbboard/src/main/java/com/example/mbboard/service/LoginService.java
@@ -12,7 +12,7 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 @Transactional
 @Service
-public class LoginSerivce implements IloginService {
+public class LoginService implements IloginService {
 	@Autowired LoginMapper loginMapper;
 	
 	@Override
