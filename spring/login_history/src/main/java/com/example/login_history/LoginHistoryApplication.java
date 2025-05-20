b7695deb49a737c1ce678dc305b46165e63d2484
hhlee7 <gdjlhh777@gmail.com>
2025-05-20 14:23:39 +0900

    프로젝트 초기 설정

---
+++ spring/login_history/src/main/java/com/example/login_history/LoginHistoryApplication.java
@@ -0,0 +1,13 @@
+package com.example.login_history;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoginHistoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoginHistoryApplication.class, args);
+	}
+
+}
---
+++ spring/login_history/src/test/java/com/example/login_history/LoginHistoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.login_history;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoginHistoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
