8afad794698d15f592d0c8e9bff6c8539b92f544
hhlee7 <gdjlhh777@gmail.com>
2025-06-30 17:17:44 +0900

    controller에 @CrossOrigin 어노테이션 추가

--- sakila-api/src/main/java/com/sakila/api/restcontroller/AddressController.java
+++ sakila-api/src/main/java/com/sakila/api/restcontroller/AddressController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
@@ -17,6 +18,7 @@ import com.sakila.api.entity.AddressEntity;
 import com.sakila.api.service.AddressService;
 
 @RestController
+@CrossOrigin
 public class AddressController {
 	private AddressService addressService;
 	
